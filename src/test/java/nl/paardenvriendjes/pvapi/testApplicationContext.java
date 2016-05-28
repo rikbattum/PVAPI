e1c2ed0aa775793e78d7e59920826efa9d5e0c47
rikbattum <rikbattum@hotmail.com>
2016-05-28 14:52:38 +0200

    update x

---
+++ src/test/java/nl/paardenvriendjes/pvapi/testApplicationContext.java
@@ -0,0 +1,14 @@
+package nl.paardenvriendjes.pvapi;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class testApplicationContext {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
